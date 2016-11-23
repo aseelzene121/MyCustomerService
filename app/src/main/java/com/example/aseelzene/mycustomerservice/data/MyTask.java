779eda58f14ce3a9d0754edc54c326208776a803
aseelzene121 <aseelz940@gmail.com>
2016-11-23 14:53:31 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	.idea/misc.xml

--- app/src/main/java/com/example/aseelzene/mycustomerservice/data/MyTask.java
+++ app/src/main/java/com/example/aseelzene/mycustomerservice/data/MyTask.java
@@ -6,9 +6,7 @@ import com.example.aseelzene.mycustomerservice.Signup;
  * Created by user on 11/15/2016.
  */
 public class MyTask {
-    private String id;
-    private String ZoneCode;
-    private String Name;
+
 
 
 }
