237307f82a04a5b135d717497ba8e7816a948c0b
aseelzene121 <aseelz940@gmail.com>
2016-11-24 11:22:51 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	.idea/misc.xml

--- app/src/main/java/com/example/aseelzene/mycustomerservice/data/MyTask.java
+++ app/src/main/java/com/example/aseelzene/mycustomerservice/data/MyTask.java
@@ -6,7 +6,67 @@ import com.example.aseelzene.mycustomerservice.Signup;
  * Created by user on 11/15/2016.
  */
 public class MyTask {
+    private String ZoneCode;
+    private String Name;
+    private String Status;
+    private  boolean isCompleted;
 
+    public MyTask(String ZoneCode, String Name, String Status) {
+        this.ZoneCode = ZoneCode;
+        this.Name = Name;
+        this.Status = Status;
+        this.isCompleted=isCompleted();
+    }
+    public MyTask()
+    {
 
+    }
+    public boolean isCompleted() {
+        return isCompleted;
+    }
+    public void setIsCompleted(boolean isCompleted){this.isCompleted=isCompleted;}
+
+    public String getZoneCode() {
+        return ZoneCode;
+    }
+
+    public void setId(String ZoneCode) {
+        this.ZoneCode=ZoneCode;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String Name) {
+        this.Name=Name;
+    }
+
+    public String getStatus() {
+        return Status;
+    }
+
+    public void setStatus(String status) {
+        this.Status = status;
+    }
+
+    @Override
+    public String toString() {
+        return "MyTask{" +
+                "ZoneCode='" + ZoneCode + '\'' +
+                ", Name='" + Name + '\'' +
+                ", status=" + Status +
+                '}';
+    }
+    public boolean setIsCompleted(){
+        if (isCompleted);
+        return true;
+
+    }
 
 }
+
+
+
+
+
