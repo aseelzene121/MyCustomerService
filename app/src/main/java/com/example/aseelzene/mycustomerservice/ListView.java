4667e6e5b82d91d9ab3ef036bf74870bb0b91508
aseelzene121 <aseelz940@gmail.com>
2016-12-01 08:52:12 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	.idea/misc.xml

--- app/src/main/java/com/example/aseelzene/mycustomerservice/ListView.java
+++ app/src/main/java/com/example/aseelzene/mycustomerservice/ListView.java
@@ -3,14 +3,69 @@ package com.example.aseelzene.mycustomerservice;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 
+import com.example.aseelzene.mycustomerservice.data.MyAdapter;
 import com.example.aseelzene.mycustomerservice.data.Request;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class ListView extends AppCompatActivity {
+    private Button rdWait;
+    private Button rdDone;
+    private Button rdOnMyWay;
+    private MyAdapter adapter;
+    private ListView listview;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_view);
+
+        rdDone = (Button) findViewById(R.id.rdDone);
+        rdWait = (Button) findViewById(R.id.rdWait);
+        rdOnMyWay = (Button) findViewById(R.id.rdOnMyWay);
+        adapter = new MyAdapter(this, R.layout.activity_list_view);
+        listview.setadapter(adapter);
+        initListView();
+
+    }
+
+    private void initListView() {
+        String email =FirebaseAuth.getInstance().getCurrentUser().getEmail().replace('.','_');
+        DatabaseReference reference= FirebaseDatabase.getInstance().getReference(email);
+        reference.child("Request").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                adapter.clear();
+                for (DatabaseReference ds:dataSnapshot.getChildren())
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        })
+    }
+
+    public void setadapter(MyAdapter adapter) {
+        this.adapter = adapter;
+    }
+    @Override
+ protected void onStart(){
+        super.onStart();
+        initListView();
+    }
 
 }
+
+
 
 
