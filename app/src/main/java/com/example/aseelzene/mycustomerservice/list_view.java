1ee5722ab29350cf86e279cd32cbe1c26e6d03f6
aseelzene121 <aseelz940@gmail.com>
2016-12-07 11:35:51 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	.idea/misc.xml

--- app/src/main/java/com/example/aseelzene/mycustomerservice/list_view.java
+++ app/src/main/java/com/example/aseelzene/mycustomerservice/list_view.java
@@ -6,8 +6,16 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+
+import com.example.aseelzene.mycustomerservice.data.MyAdapter;
 
 public class list_view extends AppCompatActivity {
+    private Button rdWait;
+    private Button rdDone;
+    private Button rdOnMyWay;
+    private MyAdapter adapter;
+    private ListView ivTasks;
 
 
     @Override
@@ -16,6 +24,12 @@ public class list_view extends AppCompatActivity {
         setContentView(R.layout.activity_list_view);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        rdDone = (Button) findViewById(R.id.rdDone);
+        rdWait = (Button) findViewById(R.id.rdWait);
+        rdOnMyWay = (Button) findViewById(R.id.rdOnMyWay);
+        adapter = new MyAdapter(this, R.layout.activity_list_view);
+
+
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
