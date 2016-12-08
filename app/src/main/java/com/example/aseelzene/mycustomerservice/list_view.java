a327c88fb7877b71054c16a2c934885afa4fbe07
aseelzene121 <aseelz940@gmail.com>
2016-12-08 08:40:26 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	.idea/misc.xml

--- app/src/main/java/com/example/aseelzene/mycustomerservice/list_view.java
+++ app/src/main/java/com/example/aseelzene/mycustomerservice/list_view.java
@@ -9,13 +9,20 @@ import android.view.View;
 import android.widget.Button;
 
 import com.example.aseelzene.mycustomerservice.data.MyAdapter;
+import com.example.aseelzene.mycustomerservice.data.Request;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class list_view extends AppCompatActivity {
     private Button rdWait;
     private Button rdDone;
     private Button rdOnMyWay;
     private MyAdapter adapter;
-    private ListView ivTasks;
+    private list_view ivTasks;
 
 
     @Override
@@ -30,7 +37,6 @@ public class list_view extends AppCompatActivity {
         adapter = new MyAdapter(this, R.layout.activity_list_view);
 
 
-
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -41,4 +47,32 @@ public class list_view extends AppCompatActivity {
         });
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        initListView();
+    }
+
+    private void initListView() {
+        String email = FirebaseAuth.getInstance().getCurrentUser().getEmail().replace('.', '_');
+        final DatabaseReference reference = FirebaseDatabase.getInstance().getReference(email);
+        reference.child("Request").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                adapter.clear();
+                for (DataSnapshot ds : dataSnapshot.getChildren()) {
+                    Request request = ds.getValue(Request.class);
+                    adapter.add(request);
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+    }
 }
+
+
+
